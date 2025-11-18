f4eec74e24d54a6511d797b308f324ee151ab952
ChinCobre <1DAM@A11-PC23>
2025-11-18 11:50:50 +0100

    mi parte

--- Reto2/src/Reto/Funciones3.java
+++ Reto2/src/Reto/Funciones3.java
@@ -2,4 +2,39 @@ package Reto;
 
 public class Funciones3 {
 
+	
+	
+	
+	public static double EuroADolar(double dinero) { //para de euro a dolar
+		return dinero*1.16;
+	}
+	
+	
+	public static double DolarAEuro(double dinero) {// para de dolar a euro
+		return dinero/1.16;
+	}
+	
+	
+	public static double GradoARadiante(double angulo) {// pasar de grado a radiante
+		return angulo*Math.PI/180;
+	}
+	
+	
+	
+	public static double RadianteAGrado(double angulo) {// pasar de radiante a grado
+		return (angulo*180)/Math.PI;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
