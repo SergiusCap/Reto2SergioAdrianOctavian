e5a9d44537178e78dec104e54f060e3115860c55
Sergio <Sergio@PCSERGIUS>
2025-11-18 11:52:03 +0100

    buenassss

--- Reto2/src/Reto/Funciones1.java
+++ Reto2/src/Reto/Funciones1.java
@@ -2,4 +2,17 @@ package Reto;
 
 public class Funciones1 {
 
+	public static double millas(double a, double b) {
+		
+		return a*b;
+	}
+	public static double kilometros(double a, double b) {
+		return a*b;
+	}
+	public static double libras(double a, double b) {
+		return a*b;
+	}
+	public static double kilogramos(double a,double b) {
+		return a*b;
+	}
 }
